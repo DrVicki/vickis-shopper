0aa1ffbfa939a7fffb6f5b79834b49394beab0fe
DrVicki <vbealman@fullsail.com>
2021-04-12 11:06:48 -0400

    1 binaries uploaded

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,22 +1,38 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Item from './Item.js';
-import './CartPage.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./CartPage.css";
 
-function CartPage({items}) {
-  return (
-    <ul className="CartPage-items">
-    {items.map(item =>
-    <li key={item.id}
-    className="CartPage-item">
-    <Item item={item} />
-    </li>)}
-    </ul>
-  );
+import Item from "./Item.js"
+
+function CartPage({items, onAddOne, onRemoveOne}) {
+    return (
+        <ul className="CartPage-items">
+            {items.map(item =>
+                <li key={item.id} className="CartPage-item">
+                    <Item item={item}>
+                        <div className="CartItem-controls">
+                            <button 
+                                className="CartItem-removeOne"
+                                onClick={() => onRemoveOne(item)}
+                            >&ndash;</button>
+                            <span className="CartItem-count">{item.count}</span>
+                            <button 
+                                className="CartItem-addOne"
+                                onClick={() => onAddOne(item)}
+                            >+</button>
+                        </div>
+                    </Item>
+                </li>    
+            )}
+
+        </ul>
+    );
 }
 
 CartPage.propTypes = {
-  items: PropTypes.array.isRequired
+    items: PropTypes.array.isRequired,
+    onAddOne: PropTypes.func.isRequired,
+    onRemoveOne: PropTypes.func.isRequired
 };
 
 export default CartPage;
--- src/Item.js
+++ src/Item.js
@@ -1,30 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Item.css';
 
-const Item = ({item, OnAddToCart}) => (
+const Item = ({item, children}) => (
   <div className="Item">
-  <div className="Item-left">
-  <div className="Item-image"></div>
-  <div className="Item-title">{item.name}
+    <div className="Item-left">
+      <div className="Item-image">
+      {item.image}</div>
+      <div className="Item-title">
+        {item.name}
+      </div>
+      <div className="Item-description">
+        {item.description}
+      </div>
+    </div>
+    <div className="Item-right">
+      <div className="Item-price">
+        ${item.price}
+      </div>
+      {children}
+    </div>
   </div>
-  <div className="Item-description">
-  {item.description}
-  </div>
-  </div>
-  <div className="Item-right">
-  <div className="Item-price">
-  ${item.price}
-  </div>
-  <button className="Item-addToCart">
-  Add to Cart</button>
-  </div>
-  </div>
-)
+);
 
 Item.propTypes = {
   item: PropTypes.object.isRequired,
-  OnAddToCart: PropTypes.func.isRequired
 };
 
 export default Item;
